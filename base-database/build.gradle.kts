4d8f894b914d77401e9824bd445a742d9740dafc
mukuljangir372 <mukul.jangir372@gmail.com>
2023-02-13 20:36:51 +0530

    base + common layer added

---
+++ base-database/build.gradle.kts
@@ -0,0 +1,9 @@
+plugins {
+    id("primer_android_lib")
+}
+android {
+    namespace = "com.mukul.jan.primer.base.database"
+}
+dependencies {
+    implementation(libs.androidx.core.ktx)
+}
---
+++ base-preferences/build.gradle.kts
@@ -0,0 +1,9 @@
+plugins {
+    id("primer_android_lib")
+}
+android {
+    namespace = "com.mukul.jan.primer.base.preferences"
+}
+dependencies {
+    implementation(libs.androidx.core.ktx)
+}
