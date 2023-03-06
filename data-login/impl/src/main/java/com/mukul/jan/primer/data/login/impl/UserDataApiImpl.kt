cd9a8e10bf5ffdbb288abd40f61a91f77f303826
mukuljangir372 <mukul.jangir372@gmail.com>
2023-03-06 19:09:42 +0530

    user data api

---
+++ data-login/impl/src/main/java/com/mukul/jan/primer/data/login/impl/UserDataApiImpl.kt
@@ -0,0 +1,9 @@
+package com.mukul.jan.primer.data.login.impl
+
+import com.mukul.jan.primer.data.login.api.UserDataApi
+
+class UserDataApiImpl : UserDataApi {
+    override suspend fun save(userId: String, save: UserDataApi.Save) {
+        TODO("Not yet implemented")
+    }
+}
