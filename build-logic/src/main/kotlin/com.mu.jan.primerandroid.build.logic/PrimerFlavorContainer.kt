868dcb74bc6cf84cdb17bf01e375e48fa3173f43
mukuljangir372 <mukul.jangir372@gmail.com>
2023-03-07 10:17:33 +0530

    login flavor entry

--- build-logic/src/main/kotlin/com.mu.jan.primerandroid.build.logic/PrimerFlavorContainer.kt
+++ build-logic/src/main/kotlin/com.mu.jan.primerandroid.build.logic/PrimerFlavorContainer.kt
@@ -4,7 +4,6 @@ data class ProductFlavor(
     val name: String, //name of the flavor
     val module: String, //flavor depend on which module
     val useSeparatePackage: Boolean, //install a separate apk if true
-    val entry: String, //root entry e.g any activity
 )
 
 class PrimerFlavorContainer {
@@ -14,19 +13,16 @@ class PrimerFlavorContainer {
                 name = "full",
                 module = ":flavor-full",
                 useSeparatePackage = false,
-                entry = ""
             ),
             ProductFlavor(
                 name = "onboarding",
                 module = ":flavor-onboarding",
                 useSeparatePackage = true,
-                entry = ""
             ),
             ProductFlavor(
                 name = "login",
                 module = ":flavor-login",
                 useSeparatePackage = true,
-                entry = "com.mukul.jan.primer.flavor.login.RootEntry"
             ),
         )
     }
