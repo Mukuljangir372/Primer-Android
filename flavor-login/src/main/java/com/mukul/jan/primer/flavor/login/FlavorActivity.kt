347e337415ea99934f0ca8fa09f5991f2117f5c7
mukuljangir372 <mukul.jangir372@gmail.com>
2023-03-07 14:55:09 +0530

    login navigation

--- flavor-login/src/main/java/com/mukul/jan/primer/flavor/login/FlavorActivity.kt
+++ flavor-login/src/main/java/com/mukul/jan/primer/flavor/login/FlavorActivity.kt
@@ -6,8 +6,10 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import com.mukul.jan.primer.base.ui.design.PrimerTheme
+import com.mukul.jan.primer.feature.login.navigation.LoginNav
 
 class FlavorActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,7 +20,15 @@ class FlavorActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize()
                 ) {
-                    FlavorComposeNav(navController)
+                    NavHost(
+                        navController = navController,
+                        startDestination = LoginNav.getRootRoute()
+                    ) {
+                        LoginNav.addLoginNavAtTopLevel(
+                            navController = navController,
+                            graph = this
+                        )
+                    }
                 }
             }
         }
