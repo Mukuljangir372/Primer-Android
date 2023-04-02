0f68bcfdd59ac28e1adb2c0aa7170797ac8edac7
mukuljangir372 <mukul.jangir372@gmail.com>
2023-04-02 05:48:48 +0530

    get user by objectId fix

--- data-login/impl/src/main/java/com/mukul/jan/primer/data/login/impl/api/UserApiImpl.kt
+++ data-login/impl/src/main/java/com/mukul/jan/primer/data/login/impl/api/UserApiImpl.kt
@@ -7,6 +7,7 @@ import com.mukul.jan.primer.data.login.api.api.model.UserRealmModel
 import com.mukul.jan.primer.data.login.api.exception.UserNotFoundException
 import com.mukul.jan.primer.data.login.api.exception.UserNotLoggedInException
 import io.realm.kotlin.UpdatePolicy
+import io.realm.kotlin.types.ObjectId
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
@@ -23,6 +24,7 @@ class UserApiImpl @Inject constructor(
         }
     }
 
+    @Suppress("DEPRECATION")
     @Throws
     override suspend fun getUser(id: String): UserRealmModel {
         return withContext(dispatchers.io) {
@@ -30,7 +32,7 @@ class UserApiImpl @Inject constructor(
             realm.query(
                 clazz = UserRealmModel::class,
                 query = "_id == $0",
-                id,
+                ObjectId.from(id),
             ).first().find() ?: throw UserNotFoundException()
         }
     }
