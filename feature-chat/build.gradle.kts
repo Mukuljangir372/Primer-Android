323de8aded7bcce375638333e4a745136abe054c
mukuljangir372 <mukul.jangir372@gmail.com>
2023-04-08 13:59:13 +0530

    feature + flavor chat

---
+++ feature-chat/build.gradle.kts
@@ -0,0 +1,21 @@
+@file:Suppress("UnstableApiUsage")
+
+plugins {
+    id("primer_android_hilt_lib")
+}
+android {
+    namespace = "com.mukul.jan.primer.feature.chat"
+    buildFeatures {
+        compose = true
+    }
+}
+dependencies {
+    implementation(project(":base-ui"))
+    implementation(project(":common"))
+    implementation(project(":common-ui"))
+
+    platform(libs.android.compose.bom)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.bundles.android.compose)
+    debugImplementation(libs.bundles.android.compose.debug)
+}
